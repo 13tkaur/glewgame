3ce17396513f586294360283587e7828ad619022
Taranjeet Singh <taranjs@users.noreply.github.com>
2023-02-04 18:25:04 +0000

    added beginner level word map

---
+++ glewmap0.js
@@ -0,0 +1,748 @@
+{
+    1: { name: "OORHAA", gurmukhi: "ੳ", words: [
+            "owl",
+            "wool",
+            "camel",
+            "wait",
+            "above",
+            "north"
+        ], gurmukhiTranslit: [
+            "ullu",
+            "oon",
+            "ootth",
+            "udeek",
+            "utte",
+            "uttar"
+        ], gurmukhiUnicode: [
+            "ਉੱਲੂ",
+            "ਊਨ",
+            "ਊਠ",
+            "ਉਡੀਕ",
+            "ਉੱਤੇ",
+            "ਉੱਤਰ"
+        ], gurmukhiTT: [
+            "au`lU",
+            "aun",
+            "au`T"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/6/D/-/-/OORHAA.gif"
+    },
+    2: { name: "AIRHAA", gurmukhi: "ਅ", words: [
+            "mango",
+            "pomegranate",
+            "pickle",
+            "newspaper",
+            "sunday",
+            "today",
+            "eye",
+            "pineapple",
+            "grapes",
+            "potato",
+            "ginger"
+        ], gurmukhiTranslit: [
+            "ammb",
+            "anaar",
+            "achaar",
+            "akhbaar",
+            "aitvaar",
+            "ajj",
+            "akkh",
+            "anaanaas",
+            "angoor",
+            "aaloo",
+            "adrak"
+        ], gurmukhiUnicode: [
+            "ਅੰਬ",
+            "ਅਨਾਰ",
+            "ਅਚਾਰ",
+            "ਅਖ਼ਬਾਰ",
+            "ਐਤਵਾਰ",
+            "ਅੱਜ",
+            "ਅੱਖ",
+            "ਅਨਾਨਾਸ",
+            "ਅੰਗੂਰ",
+            "ਆਲੂ",
+            "ਅਦਰਕ"
+        ], image: "http://0.tqn.com/d/sikhism/1/0/7/D/-/-/AIRHAA.gif"
+    },
+    3: { name: "EERHEE", gurmukhi: "ੲ", words: [
+            "eagle",
+            "brick",
+            "tamarind",
+            "one",
+            "unity",
+            "honesty"
+        ], gurmukhiTranslit: [
+            "ill",
+            "itt",
+            "imli",
+            "ikk",
+            "ekta",
+            "imaandaari"
+        ], gurmukhiUnicode: [
+            "ਇੱਲ",
+            "ਇੱਟ",
+            "ਇਮਲੀ",
+            "ਇੱਕ",
+            "ਏਕਤਾ",
+            "ਇਮਾਨਦਾਰੀ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/8/D/-/-/EERHEE.gif"
+    },
+    4: { name: "SASSAA", gurmukhi: "ਸ", words: [
+            "snake",
+            "apple",
+            "orange",
+            "soap",
+            "whistle",
+            "road",
+            "dry",
+            "beautiful",
+            "pig",
+            "head",
+            "monday"
+        ], gurmukhiTranslit: [
+            "sapp",
+            "seb",
+            "santra",
+            "saaban",
+            "seeti",
+            "sarak",
+            "sukka",
+            "sohna",
+            "soor",
+            "sir",
+            "somvaar"
+        ], gurmukhiUnicode: [
+            "ਸੱਪ",
+            "ਸੇਬ ",
+            "ਸੰਤਰਾ",
+            "ਸਾਬਣ",
+            "ਸੀਟੀ",
+            "ਸੜਕ",
+            "ਸੁੱਕਾ",
+            "ਸੋਹਣਾ",
+            "ਸੂਰ",
+            "ਸਿਰ",
+            "ਸੋਮਵਾਰ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/q/B/-/-/SASSAA.gif"
+    },
+    5: { name: "HAAHAA", gurmukhi: "ਹ", words: [
+            "elephant",
+            "swan",
+            "necklace",
+            "bone",
+            "hand",
+            "deer"
+        ], gurmukhiTranslit: [
+            "haathi",
+            "hans",
+            "haar",
+            "haddee",
+            "hathth",
+            "hiran"
+        ], gurmukhiUnicode: [
+            "ਹਾਥੀ",
+            "ਹੰਸ",
+            "ਹਾਰ",
+            "ਹੱਡੀ",
+            "ਹੱਥ",
+            "ਹਿਰਣ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/6/C/-/-/HAAHAA.gif"
+    },
+    6: { name: "KAKKAA", gurmukhi: "ਕ", words: [
+            "bowl",
+            "hair",
+            "comb",
+            "pigeon",
+            "lotus",
+            "dog",
+            "blanket",
+            "wheat",
+            "paper",
+            "chair",
+            "ant",
+            "prisoner",
+            "work",
+            "book",
+            "ear",
+            "loan"
+        ], gurmukhiTranslit: [
+            "katora",
+            "kes",
+            "kangha",
+            "kabootar",
+            "kamal",
+            "kutta",
+            "kambal",
+            "kanak",
+            "kaagaz",
+            "kursi",
+            "keeree",
+            "kaidi",
+            "kam",
+            "kitaab",
+            "kann",
+            "karza"
+        ], gurmukhiUnicode: [
+            "ਕਟੋਰਾ",
+            "ਕੇਸ",
+            "ਕੰਘਾ",
+            "ਕਬੂਤਰ",
+            "ਕਮਲ",
+            "ਕੁੱਤਾ",
+            "ਕਂਬਲ",
+            "ਕਣਕ",
+            "ਕਾਗਜ਼",
+            "ਕੁਰਸੀ",
+            "ਕੀੜੀ",
+            "ਕੈਦੀ",
+            "ਕੰਮ",
+            "ਕਿਤਾਬ",
+            "ਕੰਨ",
+            "ਕਰਜ਼ਾ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/9/C/-/-/KAKKAA.gif"
+    },
+    7: { name: "KHAKHKHAA", gurmukhi: "ਖ", words: [
+            "rabbit",
+            "cucumber",
+            "well",
+            "toy",
+            "donkey"
+        ], gurmukhiTranslit: [
+            "khargosh",
+            "kheera",
+            "khooh",
+            "khidona",
+            "khotaa"
+        ], gurmukhiUnicode: [
+            "ਖਰਗੋਸ਼",
+            "ਖੀਰਾ",
+            "ਖੂਹ",
+            "ਖਿਡੋਣਾ",
+            "ਖੋਤਾ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/A/C/-/-/KHAKHKHAA.gif"
+    },
+    8: { name: "GAGGAA", gurmukhi: "ਗ", words: [
+            "cow",
+            "carrot",
+            "car",
+            "balloon",
+            "poor",
+            "knowledge",
+            "ball"
+        ], gurmukhiTranslit: [
+            "gaa(n)",
+            "gaajar",
+            "gaddi",
+            "gubaara",
+            "gareeb",
+            "giaan",
+            "gend"
+        ], gurmukhiUnicode: [
+            "ਗਾਂ",
+            "ਗਾਜਰ",
+            "ਗੱਡੀ",
+            "ਗੁਬਾਰਾ",
+            "ਗਰੀਬ",
+            "ਗਿਆਨ",
+            "ਗੇਂਦ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/4/C/-/-/GAGGAA.gif"
+    },
+    9: { name: "GHAGHGHAA", gurmukhi: "ਘ", words: [
+            "horse",
+            "home",
+            "earthen-pot",
+            "bell",
+            "hour"
+        ], gurmukhiTranslit: [
+            "ghorha",
+            "ghar",
+            "gharha",
+            "ghanti",
+            "ghantaa"
+        ], gurmukhiUnicode: [
+            "ਘੋੜਾ",
+            "ਘਰ",
+            "ਘੜਾ",
+            "ਘੰਟੀ",
+            "ਘੰਟਾ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/5/C/-/-/GHAGHGHAA.gif"
+    },
+    10: { name: "SHASSAA", gurmukhi: "ਸ਼", words: [
+            "lion",
+            "mirror"
+        ], gurmukhiTranslit: [
+            "sher",
+            "sheesha"
+        ], gurmukhiUnicode: [
+            "ਸ਼ੇਰ",
+            "ਸ਼ੀਸ਼ਾ"
+        ]
+    },
+    11: { name: "CHACHCHAA", gurmukhi: "ਚ", words: [
+            "mouse",
+            "key",
+            "spoon",
+            "rice",
+            "moon",
+            "thief",
+            "climb",
+            "tea",
+            "sparrow"
+        ], gurmukhiTranslit: [
+            "chooha",
+            "chaabi",
+            "chammach",
+            "chaawal",
+            "chann",
+            "chor",
+            "chardna",
+            "chaah",
+            "chiree"
+        ], gurmukhiUnicode: [
+            "ਚੂਹਾ",
+            "ਚਾਬੀ",
+            "ਚੱਮਚ",
+            "ਚਾਵਲ",
+            "ਚੰਨ",
+            "ਚੋਰ",
+            "ਚੜਣਾ",
+            "ਚਾਹ",
+            "ਚਿੜੀ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/z/B/-/-/CHACHCHAA.gif"
+    },
+    12: { name: "CHHACHHCHHAA", gurmukhi: "ਛ", words: [
+            "umbrella",
+            "six",
+            "roof",
+            "peel"
+        ], gurmukhiTranslit: [
+            "chhaata",
+            "chhey",
+            "chhat",
+            "chhilka"
+        ], gurmukhiUnicode: [
+            "ਛਾਤਾ",
+            "ਛੇ",
+            "ਛਤ",
+            "ਛਿਲਕਾ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/-/C/-/-/CHHACHHCHHAA.gif"
+    },
+    13: { name: "JAJJAA", gurmukhi: "ਜ", words: [
+            "ship",
+            "forest",
+            "rust",
+            "floor",
+            "pocket",
+            "web",
+            "prison",
+            "class"
+        ], gurmukhiTranslit: [
+            "jahaaz",
+            "jangal",
+            "jang",
+            "zameen",
+            "jeb",
+            "jaala",
+            "jel",
+            "jamaat"
+        ], gurmukhiUnicode: [
+            "ਜਹਾਜ਼",
+            "ਜੰਗਲ",
+            "ਜੰਗ",
+            "ਜ਼ਮੀਨ",
+            "ਜੇਬ",
+            "ਜਾਲਾ",
+            "ਜੇਲ",
+            "ਜਮਾਤ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/7/C/-/-/JAJJAA.gif"
+    },
+    14: { name: "JHAJHJHAA", gurmukhi: "ਝ", words: [
+            "flag",
+            "waterfall",
+            "broom"
+        ], gurmukhiTranslit: [
+            "jhanda",
+            "jharna",
+            "jhaadu"
+        ], gurmukhiUnicode: [
+            "ਝੰਡਾ",
+            "ਝਰਨਾ",
+            "ਝਾੜੂ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/8/C/-/-/JHAJHJHAA.gif"
+    }, xx: { name: "NJANNJAA", gurmukhi: "ਞ", words: [
+            "xx",
+            "xx"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/G/C/-/-/NJANNJAA.gif"
+    },
+    16: { name: "TAINKAA", gurmukhi: "ਟ", words: [
+            "tomato"
+        ], gurmukhiTranslit: [
+            "tamaatar"
+        ], gurmukhiUnicode: [
+            "ਟਮਾਟਰ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/w/B/-/-/TAINKAA.gif"
+    },
+    17: { name: "TtHATtHTtHAA", gurmukhi: "ਠ", words: [
+            "stamp",
+            "cold"
+        ], gurmukhiTranslit: [
+            "tthappa",
+            "thhanda"
+        ], gurmukhiUnicode: [
+            "ਠੱਪਾ",
+            "ਠੰਡਾ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/t/B/-/-/TtHATtHTtHAA.gif"
+    },
+    18: { name: "DdADdDdAA", gurmukhi: "ਡ", words: [
+            "frog",
+            "box"
+        ], gurmukhiTranslit: [
+            "daddu",
+            "dabba"
+        ], gurmukhiUnicode: [
+            "ਡੱਡੂ",
+            "ਡੱਬਾ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/1/C/-/-/DdADdDdAA.gif"
+    },
+    19: { name: "DdHADdHDdHAA", gurmukhi: "ਢ", words: [
+            "drum",
+            "lid",
+            "shield"
+        ], gurmukhiTranslit: [
+            "ddhol",
+            "ddhakkan",
+            "ddhaal"
+        ], gurmukhiUnicode: [
+            "ਢੋਲ",
+            "ਢੱਕਣ",
+            "ਢਾਲ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/2/C/-/-/DdHADdHDdHAA.gif"
+    }, xx: { name: "NHAANHAA", gurmukhi: "ਣ", words: [
+            "xx",
+            "xx"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/F/C/-/-/NHAANHAA.gif"
+    },
+    21: { name: "TATAA", gurmukhi: "ਤ", words: [
+            "fast",
+            "parrot",
+            "three",
+            "lock",
+            "clap",
+            "walk",
+            "photo",
+            "water-melon"
+        ], gurmukhiTranslit: [
+            "tez",
+            "tota",
+            "tinn",
+            "taala",
+            "taali",
+            "turna",
+            "tasveer",
+            "tarbooz"
+        ], gurmukhiUnicode: [
+            "ਤੇਜ਼",
+            "ਤੋਤਾ",
+            "ਤਿੰਨ",
+            "ਤਾਲਾ",
+            "ਤਾਲੀ",
+            "ਤੁਰਨਾ",
+            "ਤਸਵੀਰ",
+            "ਤਰਬੂਜ਼"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/r/B/-/-/TATAA.gif"
+    },
+    22: { name: "THATHATHAA", gurmukhi: "ਥ", words: [
+            "plate",
+            "tired"
+        ], gurmukhiTranslit: [
+            "thaal",
+            "thhakna"
+        ], gurmukhiUnicode: [
+            "ਥਾਲ",
+            "ਥਕਣਾ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/s/B/-/-/THATHATHAA.gif"
+    },
+    23: { name: "DADDAA", gurmukhi: "ਦ", words: [
+            "inkpot",
+            "turban",
+            "grandfather",
+            "grandmother",
+            "milk",
+            "heart",
+            "south",
+            "pulse"
+        ], gurmukhiTranslit: [
+            "davaat",
+            "dastaar",
+            "daada",
+            "daadi",
+            "dudh",
+            "dil",
+            "dakkhan",
+            "daal"
+        ], gurmukhiUnicode: [
+            "ਦਵਾਤ",
+            "ਦਸਤਾਰ",
+            "ਦਾਦਾ",
+            "ਦਾਦੀ",
+            "ਦੁੱਧ",
+            "ਦਿਲ",
+            "ਦੱਖਣ",
+            "ਦਾਲ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/0/C/-/-/DADDAA.gif"
+    },
+    24: { name: "DHADHDHAA", gurmukhi: "ਧ", words: [
+            "earth",
+            "thread",
+            "wash",
+            "daughter"
+        ], gurmukhiTranslit: [
+            "dharti",
+            "dhaaga",
+            "dhona",
+            "dhee"
+        ], gurmukhiUnicode: [
+            "ਧਰਤੀ",
+            "ਧਾਗਾ",
+            "ਧੋਣਾ",
+            "ਧੀ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/3/C/-/-/DHADHDHAA.gif"
+    },
+    25: { name: "NANNAA", gurmukhi: "ਨ", words: [
+            "measure",
+            "lemon",
+            "nose",
+            "pear"
+        ], gurmukhiTranslit: [
+            "naapna",
+            "nimbu",
+            "nakk",
+            "naakh"
+        ], gurmukhiUnicode: [
+            "ਨਾਪਣਾ",
+            "ਨੀਂਬੂ",
+            "ਨੱਕ",
+            "ਨਾਖ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/D/C/-/-/NANNAA.gif"
+    },
+    26: { name: "PAPPAA", gurmukhi: "ਪ", words: [
+            "leaf",
+            "bird",
+            "five",
+            "son",
+            "sin",
+            "wings",
+            "father",
+            "generation",
+            "bridge",
+            "love",
+            "swing",
+            "east",
+            "west"
+        ], gurmukhiTranslit: [
+            "patta",
+            "panchhi",
+            "panj",
+            "puttar",
+            "paap",
+            "pankh/par",
+            "pita/pyo",
+            "paneeri",
+            "pul",
+            "prem/pyaar",
+            "peengh",
+            "poorab",
+            "pachcham"
+        ], gurmukhiUnicode: [
+            "ਪੱਤਾ",
+            "ਪੰਛੀ",
+            "ਪੰਜ",
+            "ਪੁੱਤਰ",
+            "ਪਾਪ",
+            "ਪੰਖ/ਪਰ",
+            "ਪਿਤਾ/ਪਿਓ",
+            "ਪਨੀਰੀ",
+            "ਪੁੱਲ",
+            "ਪ੍ਰੇਮ/ਪਿਆਰ",
+            "ਪੀਂਘ",
+            "ਪੂਰਬ",
+            "ਪੱਛਮ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/H/C/-/-/PAPPAA.gif"
+    },
+    27: { name: "PHAPHPHAA", gurmukhi: "ਫ", words: [
+            "fruit",
+            "flower",
+            "army"
+        ], gurmukhiTranslit: [
+            "phal",
+            "phul",
+            "fauj"
+        ], gurmukhiUnicode: [
+            "ਫਲ",
+            "ਫੁੱਲ",
+            "ਫੌਜ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/I/C/-/-/PHAPHPHAA.gif"
+    },
+    28: { name: "BABBAA", gurmukhi: "ਬ", words: [
+            "cat",
+            "duck",
+            "speak",
+            "garden",
+            "monkey",
+            "kid",
+            "wednesday",
+            "almond",
+            "goat",
+            "wolf"
+        ], gurmukhiTranslit: [
+            "billi",
+            "battakh",
+            "bolna",
+            "baagh/bageecha",
+            "baandar",
+            "bachha",
+            "budhvaar",
+            "badaam",
+            "bakkaree",
+            "baghiaar"
+        ], gurmukhiUnicode: [
+            "ਬਿੱਲੀ",
+            "ਬਤਖ਼",
+            "ਬੋਲਣਾ",
+            "ਬਾਘ/ਬਗੀਚਾ",
+            "ਬਾਂਦਰ",
+            "ਬੱਚਾ",
+            "ਬੁੱਧਵਾਰ",
+            "ਬਦਾਮ",
+            "ਬਕਰੀ",
+            "ਬਘਿਆੜ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/x/B/-/-/BABBAA.gif"
+    },
+    29: { name: "BHABBHAA", gurmukhi: "ਭ", words: [
+            "bear",
+            "sheep",
+            "forget",
+            "sister",
+            "crowd"
+        ], gurmukhiTranslit: [
+            "bhaalu",
+            "bhed",
+            "bhulna",
+            "bhain",
+            "bheer"
+        ], gurmukhiUnicode: [
+            "ਭਾਲੂ",
+            "ਭੇਡ",
+            "ਭੁੱਲਣਾ",
+            "ਭੈਣ",
+            "ਭੀੜ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/y/B/-/-/BHABBHAA.gif"
+    },
+    30: { name: "MAMMAA", gurmukhi: "ਮ", words: [
+            "fish",
+            "peacock",
+            "my",
+            "table",
+            "temple",
+            "mother",
+            "forehead",
+            "shoulder",
+            "tuesday",
+            "butter"
+        ], gurmukhiTranslit: [
+            "machhli",
+            "mor",
+            "mera",
+            "mez",
+            "mandar",
+            "maa/maata",
+            "mathha",
+            "maudda",
+            "mangalvaar",
+            "makkhan"
+        ], gurmukhiUnicode: [
+            "ਮਛਲੀ",
+            "ਮੋਰ",
+            "ਮੇਰਾ",
+            "ਮੇਜ਼",
+            "ਮੰਦਰ",
+            "ਮਾਂ/ਮਾਤਾ",
+            "ਮੱਥਾ",
+            "ਮੋਢਾ",
+            "ਮੰਗਲਵਾਰ",
+            "ਮੱਖਣ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/C/C/-/-/MAMMAA.gif"
+    },
+    31: { name: "YAYYA", gurmukhi: "ਯ", words: [
+            "memorial"
+        ], gurmukhiTranslit: [
+            "yaadgaar"
+        ], gurmukhiUnicode: [
+            "ਯਾਦਗਾਰ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/v/B/-/-/YAYYA.gif"
+    },
+    32: { name: "RAARAA", gurmukhi: "ਰ", words: [
+            "color",
+            "night",
+            "way",
+            "king",
+            "quilt"
+        ], gurmukhiTranslit: [
+            "rang",
+            "raat",
+            "raah",
+            "raaja",
+            "rajaai"
+        ], gurmukhiUnicode: [
+            "ਰੰਗ",
+            "ਰਾਤ",
+            "ਰਾਹ",
+            "ਰਾਜਾ",
+            "ਰਜਾਈ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/J/C/-/-/RAARAA.gif"
+    },
+    33: { name: "LALLAA", gurmukhi: "ਲ", words: [
+            "fox",
+            "garlic",
+            "essay",
+            "write"
+        ], gurmukhiTranslit: [
+            "lomrhi",
+            "lassan",
+            "lekh",
+            "likhna"
+        ], gurmukhiUnicode: [
+            "ਲੋਮੜੀ",
+            "ਲੱਸਣ",
+            "ਲੇਖ",
+            "ਲਿਖਣਾ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/B/C/-/-/LALLAA.gif"
+    },
+    34: { name: "VAAVAA", gurmukhi: "ਵ", words: [
+            "calf",
+            "wow",
+            "bangles",
+            "harmonium",
+            "twenty",
+            "thursday"
+        ], gurmukhiTranslit: [
+            "vachha",
+            "vaah",
+            "vangaa(n)",
+            "vaaja",
+            "veeh",
+            "veervaar"
+        ], gurmukhiUnicode: [
+            "ਵੱਛਾ",
+            "ਵਾਹ",
+            "ਵੰਗਾਂ",
+            "ਵਾਜਾ",
+            "ਵੀਹ",
+            "ਵੀਰਵਾਰ"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/u/B/-/-/VAAVAA.gif"
+    }, xx: { name: "RHAARHAA", gurmukhi: "ੜ", words: [
+            "xx",
+            "xx"
+        ], image: "http://0.tqn.com/d/sikhism/1/7/p/B/-/-/RHAARHAA.gif"
+    }
+}
